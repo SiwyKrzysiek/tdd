8dcd800700160261bf1451c8c007ad85b5e46d64
WinCubeML <mati031398@gmail.com>
2019-11-09 13:20:00 +0100

    Node (wersja init)
    Dodanie wartości przechowywanych w obiekcie, jak również getterów i setterów i konstruktorów (do uzgodnienia)

---
+++ Code/src/main/java/Node.java
@@ -0,0 +1,36 @@
+import java.util.List;
+
+public class Node {
+	private int id;
+	private List<Node> neighbours;
+	private int weightStorage;
+
+	public Node() {
+	}
+
+	public Node(int id) {
+		this.id = id;
+		this.weightStorage = 0;
+	}
+
+	public Node(int id, int weightStorage) {
+		this.id = id;
+		this.weightStorage = weightStorage;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getWeightStorage() {
+		return weightStorage;
+	}
+
+	public void setWeightStorage(int weightStorage) {
+		this.weightStorage = weightStorage;
+	}
+}
