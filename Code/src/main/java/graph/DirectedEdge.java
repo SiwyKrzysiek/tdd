5e6add5086b7cb89f2645e1c81d2b8465f51738b
WinCubeML <mati031398@gmail.com>
2019-11-11 00:44:01 +0100

    WeightedGraph: Weighted edges

--- Code/src/main/java/graph/DirectedEdge.java
+++ Code/src/main/java/graph/DirectedEdge.java
@@ -1,7 +1,49 @@
 package graph;
 
-public class DirectedEdge<T> {
-	public void doNothing(){
+import java.util.OptionalInt;
 
+public class DirectedEdge<T> {
+	private final Node<T> a;
+	private final Node<T> b;
+	private final OptionalInt weight;
+
+	public Node<T> getA() {
+		return a;
+	}
+
+	public Node<T> getB() {
+		return b;
+	}
+
+	public OptionalInt getWeight() {
+		return weight;
+	}
+
+	public DirectedEdge(Node<T> a, Node<T> b) {
+		this(a,b,OptionalInt.empty());
+	}
+
+	public DirectedEdge(Node<T> a, Node<T> b, int weight) {
+		this(a,b,OptionalInt.of(weight));
+	}
+
+	private DirectedEdge(Node<T> a, Node<T> b, OptionalInt weight){
+		this.a = a;
+		this.b = b;
+		this.weight = weight;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		DirectedEdge<?> that = (DirectedEdge<?>) o;
+		return (this.a == that.a && this.b == that.b)
+				|| (this.a == that.b && this.b == that.a) ;
+	}
+
+	@Override
+	public int hashCode() {
+		return a.hashCode() + b.hashCode();
 	}
 }
--- Code/src/test/java/graph/DirectedEdgeTest.java
+++ Code/src/test/java/graph/DirectedEdgeTest.java
@@ -3,17 +3,17 @@ package graph;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DirectedEdgeTest {
 	@Test
-	public void doNothing(){
+	 void directedEdgesCreationTest(){
 		//Given
 		Node<Integer> node1 = new Node<>(5);
 		Node<Integer> node2 = new Node<>(7);
 
 		//When
 		DirectedEdge<Integer> a = new DirectedEdge<>(node1, node2);
+
 		//Then
 		assertNotNull(a);
 	}
