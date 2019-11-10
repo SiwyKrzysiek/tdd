bf3ee7305de2989925130d33c40404bc89fb5461
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2019-11-10 09:36:26 +0100

    Przeniesienie testów związanych z grafem

--- Code/src/test/java/graph/GraphTest.java
+++ Code/src/test/java/graph/GraphTest.java
@@ -1,4 +1,6 @@
-import static org.junit.jupiter.api.Assertions.*;
+package graph;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class GraphTest {
 	@org.junit.jupiter.api.Test
--- Code/src/test/java/graph/NodeTest.java
+++ Code/src/test/java/graph/NodeTest.java
@@ -1,7 +1,8 @@
-import graph.Node;
+package graph;
+
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class NodeTest {
 
