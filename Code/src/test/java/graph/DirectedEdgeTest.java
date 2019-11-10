89b5287343965f63f84710f9db2e1119849736d9
WinCubeML <mati031398@gmail.com>
2019-11-11 00:10:50 +0100

    WeightedGraph init of branch

---
+++ Code/src/test/java/graph/DirectedEdgeTest.java
@@ -0,0 +1,12 @@
+package graph;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DirectedEdgeTest {
+	@Test
+	public void doNothing(){
+		assertTrue(true);
+	}
+}
