a410bf4793ab17df95914ca63c6a60b6ab6bc302
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2019-11-10 10:00:21 +0100

    TDD - Test dodawania wieszchołka o tym samy id

--- Code/src/test/java/graph/GraphTest.java
+++ Code/src/test/java/graph/GraphTest.java
@@ -1,10 +1,38 @@
 package graph;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class GraphTest {
 	@org.junit.jupiter.api.Test
 	void main() {
 		assertTrue(true);
+	}
+
+    @Test
+    void addVertexWithDifferentID() {
+		// Given
+		Graph<Integer> graph = new Graph<>();
+
+		// When
+		graph.addVertex(new Node<Integer>(0, 5));
+		graph.addVertex(new Node<Integer>(1, 7));
+
+		// Then no exception
+    }
+
+	@Test
+	void addVertexWithSameID_ThrowsException() {
+		// Given
+		Graph<Integer> graph = new Graph<>();
+
+		// When
+		graph.addVertex(new Node<Integer>(0, 5));
+
+		//Then
+		assertThrows(IllegalArgumentException.class , () -> graph.addVertex(new Node<Integer>(0, 7)));
 	}
 }
