96e9ae2c97900691fb3434ee451f61b6f4f9e0c0
WinCubeML <mati031398@gmail.com>
2019-11-11 17:53:28 +0100

    Bellman-Ford test preparation 3

--- Code/src/main/java/algorythm/BellmanFord.java
+++ Code/src/main/java/algorythm/BellmanFord.java
@@ -1,7 +1,21 @@
 package algorythm;
 
-public class BellmanFord {
-	public void doNothing() {
+import graph.Node;
 
+import java.util.Iterator;
+
+public class BellmanFord<T> implements Iterator<Node<T>> {
+	public BellmanFord(Node<T> initialNode) {
+
+	}
+
+	@Override
+	public boolean hasNext() {
+		return false;
+	}
+
+	@Override
+	public Node<T> next() {
+		return null;
 	}
 }
