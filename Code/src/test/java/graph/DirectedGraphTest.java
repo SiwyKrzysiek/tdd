b456a748f78ac30bb4a6d46bc7dd7d7ec76f6412
WinCubeML <mati031398@gmail.com>
2019-11-11 01:03:55 +0100

    WeightedGraph: Directed Graph init

---
+++ Code/src/test/java/graph/DirectedGraphTest.java
@@ -0,0 +1,11 @@
+package graph;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class DirectedGraphTest {
+	@Test
+	void doNothing(){
+		assertTrue(true);
+	}
+}
