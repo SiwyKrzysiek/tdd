051e5c8ffd214a8109e4afc289849e4e81b449fd
WinCubeML <mati031398@gmail.com>
2019-11-11 17:47:05 +0100

    Bellman-Ford test preparation

--- Code/src/test/java/algorythm/BellmanFordTest.java
+++ Code/src/test/java/algorythm/BellmanFordTest.java
@@ -1,12 +1,52 @@
 package algorythm;
 
+import graph.DirectedGraph;
+import graph.Node;
 import org.junit.jupiter.api.Test;
+import util.InfiniteCounter;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.Iterator;
 
 class BellmanFordTest {
 	@Test
-	public void doNothing() {
-		assertTrue(true);
+	public void bellmanFordTest() {
+		//Given
+		Iterator<Integer> idGenerator = new InfiniteCounter();
+
+		Node<String> s = new Node<>(idGenerator.next(), "s", 0);
+		Node<String> a = new Node<>(idGenerator.next(), "a", Integer.MAX_VALUE);
+		Node<String> b = new Node<>(idGenerator.next(), "b", Integer.MAX_VALUE);
+		Node<String> c = new Node<>(idGenerator.next(), "c", Integer.MAX_VALUE);
+		Node<String> d = new Node<>(idGenerator.next(), "d", Integer.MAX_VALUE);
+		Node<String> e = new Node<>(idGenerator.next(), "e", Integer.MAX_VALUE);
+
+		/* Graph structure
+
+			   [s]
+			  /  \
+			(8)  (10)
+			/      \
+		   V        -----------\
+		  [e]		           V
+		   |         -------> [a]
+		   |        /        /  ^--
+		  (1)   (-4)        /      \
+		   |  --/         (2)       |
+		   V /           /         (1)
+		  [d]           /           |
+		    \          V            |
+		     --(-1)->[c]----(-2)-->[b]
+
+		   [X] -- node name
+		   (y) -- edge weight
+		   - | / \  -- edges
+		   > < ^ V -- edge direction
+		*/
+
+		DirectedGraph<String> graph = new DirectedGraph<>();
+
+		//When
+
+		//Then
 	}
 }
