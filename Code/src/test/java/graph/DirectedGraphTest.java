12fb183f2efa2ff8adaf6b56cef2a371e0611792
WinCubeML <mati031398@gmail.com>
2019-11-11 01:23:10 +0100

    WeightedGraph: Test of Directed Graph

--- Code/src/test/java/graph/DirectedGraphTest.java
+++ Code/src/test/java/graph/DirectedGraphTest.java
@@ -1,11 +1,46 @@
 package graph;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class DirectedGraphTest {
 	@Test
-	void doNothing(){
-		assertTrue(true);
+	void addVertexWithDifferentID() {
+		// Given
+		DirectedGraph<Integer> graph = new DirectedGraph<>();
+
+		// When
+		graph.addVertex(new Node<Integer>(0, 5));
+		graph.addVertex(new Node<Integer>(1, 7));
+
+		// Then no exception
+	}
+
+	@Test
+	void addVertexWithSameID_ThrowsException() {
+		// Given
+		DirectedGraph<Integer> graph = new DirectedGraph<>();
+
+		// When
+		graph.addVertex(new Node<Integer>(0, 5));
+
+		//Then
+		assertThrows(IllegalArgumentException.class, () -> graph.addVertex(new Node<Integer>(0, 7)));
+	}
+
+	@Test
+	void addEdge() {
+		//Given
+		DirectedGraph<Integer> graph = new DirectedGraph<>();
+		Node<Integer> a = new Node<Integer>(0, 5);
+		Node<Integer> b = new Node<Integer>(1, 7);
+		graph.addVertex(a);
+		graph.addVertex(b);
+
+		//When
+		graph.createEdge(a, b);
+
+		//Then no exception
 	}
 }
