54617f88e61e55168b3cec8c467ef6c160033afc
javaProjekty <2t45cd6hH>
2019-11-12 01:45:36 +0100

    cos tam probuje

--- Code/src/main/java/algorythm/Dijkstra.java
+++ Code/src/main/java/algorythm/Dijkstra.java
@@ -1,4 +1,82 @@
 package algorythm;
 
-public class Dijkstra {
-}
+import java.lang.*;
+
+class Dijkstra {
+
+    static final int V = 9;
+    int minimalnyDystans(int dystans[], Boolean sptSet[])
+    {
+        // Initialize min value 
+        int min = Integer.MAX_VALUE, min_index = -1;
+
+        for (int v = 0; v < V; v++)
+            if (sptSet[v] == false && dystans[v] <= min) {
+                min = dystans[v];
+                min_index = v;
+            }
+
+        return min_index;
+    }
+
+
+    void wypisz(int dystans[])
+    {
+        System.out.println("Vertex \t\t Dystans");
+        for (int i = 0; i < V; i++)
+            System.out.println(i + " \t\t " + dystans[i]);
+    }
+
+
+    void dijkstra(int graf[][], int src)
+    {
+        int dystanss[] = new int[V];
+
+        Boolean sptSet[] = new Boolean[V];
+
+
+        for (int i = 0; i < V; i++) {
+            dystanss[i] = Integer.MAX_VALUE;
+            sptSet[i] = false;
+        }
+
+
+        dystanss[src] = 0;
+
+
+        for (int count = 0; count < V - 1; count++) {
+
+            int u = minimalnyDystans(dystanss, sptSet);
+
+
+            sptSet[u] = true;
+
+
+            for (int v = 0; v < V; v++)
+
+
+                if (!sptSet[v] && graf[u][v] != 0 && dystanss[u] != Integer.MAX_VALUE && dystanss[u] + graf[u][v] < dystanss[v])
+                    dystanss[v] = dystanss[u] + graf[u][v];
+        }
+
+
+        wypisz(dystanss);
+    }
+
+
+    public static void main(String[] args)
+    {
+
+        int graph[][] = new int[][] { { 1, 4, 0, 0, 0, 0, 0, 8, 0 },
+                { 4, 0, 8, 0, 0, 0, 0, 11, 0 },
+                { 0, 8, 0, 7, 0, 4, 0, 0, 2 },
+                { 0, 0, 7, 0, 9, 14, 0, 0, 0 },
+                { 0, 0, 0, 9, 0, 10, 0, 0, 0 },
+                { 0, 0, 4, 14, 10, 0, 2, 0, 0 },
+                { 0, 0, 0, 0, 0, 2, 0, 1, 6 },
+                { 8, 11, 0, 0, 0, 0, 1, 0, 7 },
+                { 0, 0, 2, 0, 0, 0, 6, 7, 0 } };
+        Dijkstra t = new Dijkstra();
+        t.dijkstra(graph, 0);
+    }}
+
